688899d63314e9667eadaef45b922924a6568fbc
dhayal <dhayal@gmail.com>
2022-08-02 12:18:43 +0000

    2nd commit

--- code.java
+++ code.java
@@ -3,7 +3,7 @@ public class Factorial {
     public static void main(String[] args) {
 
         int num = 10;
-        long factorial = 1;
+        long factorial = 31;
         for(int i = 1; i <= num; ++i)
         {
             // factorial = factorial * i;
